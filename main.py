80b197221e20a00fcc63081d6ddedd43c2fc426e
Chris Strutton <chris@rodandfly.ca>
2022-02-04 14:38:46 -0500

    proof of concept works

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from pylogix import PLC
 
 if __name__ == '__main__':
     with PLC() as comm:
-        comm.IPAddress = '192.168.1.9'
+        comm.IPAddress = '192.168.10.1'
         read = True
         while read:
             try:
                 ret = comm.Read('Verify_Barcode')
+                print(ret)
                 if ret.Value:
                     ret = comm.Read('Barcode_String')
                     print(ret.Value)
-                    ret = comm.Write('Barcode_OK', True)
+                    ret = comm.Write('Barcode_OK', 1)
+                    print(ret)
                 time.sleep(.5)
 
             except KeyboardInterrupt:
