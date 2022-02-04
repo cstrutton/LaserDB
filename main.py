1230a873436490ca6a0248d866155f9fc0e07e38
Chris Strutton <chris@rodandfly.ca>
2022-02-04 13:18:23 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from datetime import time
+
+from pylogix import PLC
+
+if __name__ == '__main__':
+    with PLC() as comm:
+        comm.IPAddress = '192.168.1.9'
+        read = True
+        while read:
+            try:
+                ret = comm.Read('Verify_Barcode')
+                if ret.Value:
+                    ret = comm.Read('Barcode_String')
+                    print(ret.Value)
+                    ret = comm.Write('Barcode_OK', True)
+                time.sleep(.5)
+
+            except KeyboardInterrupt:
+                print('exiting')
+                read = False
